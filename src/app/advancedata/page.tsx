fc702462904a1aeb68cc0fcc146c8e7b3cd8bb59
sohelkhan <mdsohelkhan418126@gmail.com>
2026-09-18 11:35:57 +0600

    fix: update data fetching URL and enhance layout structure

--- src/app/advancedata/page.tsx
+++ src/app/advancedata/page.tsx
@@ -6,10 +6,12 @@ type User = {
 };
 
 const page = async () => {
-  const res = await fetch('http://localhost:5000/products');
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    cache: 'force-cache',
+  });
   const posts: User[] = await res.json();
   return (
-    <div>
+    <div className="grid grid-cols-3">
       <h1>{posts.length}</h1>
       {posts.map(post => (
         <Data key={post.id} post={Promise.resolve(post)}></Data>
